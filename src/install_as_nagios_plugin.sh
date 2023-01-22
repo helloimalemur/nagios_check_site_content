d8ef56c4ae127bb9b0ddc22db42df9dd67add2fd
James Koonts <jkoontsiii@gmail.com>
2023-01-22 09:10:45 -0500

    .

--- src/install_as_nagios_plugin.sh
+++ src/install_as_nagios_plugin.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ -n /usr/local/nagios/libexec/ ]]; then
-  echo "dir '/usr/local/nagios/libexec/' does not exist";
+if [[ -f /usr/local/nagios/libexec/check_disk ]]; then
+  #  rm -rf /tmp/notify_via_discord/ 2>/dev/null;
+    cd /tmp/;
+    git clone https://github.com/helloimalemur/nagios_check_site_content.git;
+    cd nagios_check_site_content/;
+    cargo build;
+    sudo cp target/debug/check_site /usr/local/nagios/libexec/;
 else
-#  rm -rf /tmp/notify_via_discord/ 2>/dev/null;
-  cd /tmp/;
-  git clone https://github.com/helloimalemur/rustyscripts.git;
-  cd rustyscripts/check_site_for_keyword/;
-  cargo build;
-  sudo cp target/debug/check_site_for_keyword /usr/local/nagios/libexec/;
+  echo "dir '/usr/local/nagios/libexec/' does not exist";
 fi;
