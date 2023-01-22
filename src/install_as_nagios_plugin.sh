75ec99aa9779f4603f73735d08c1a647584d569f
James Koonts <jkoontsiii@gmail.com>
2023-01-22 09:42:09 -0500

    .

--- src/install_as_nagios_plugin.sh
+++ src/install_as_nagios_plugin.sh
@@ -6,7 +6,10 @@ if [[ -f /usr/local/nagios/libexec/check_disk ]]; then
     git clone https://github.com/helloimalemur/nagios_check_site_content.git;
     cd nagios_check_site_content/;
     cargo build;
-    sudo cp target/debug/check_site /usr/local/nagios/libexec/;
+    cp target/debug/check_site /usr/local/nagios/libexec/;
+    cp /tmp/nagios_check_site_content/src/examples/check_elasticsearch.sh /usr/local/nagios/libexec/;
+    cp /tmp/nagios_check_site_content/src/examples/check_kibana.sh /usr/local/nagios/libexec/;
+    chmod +x /usr/local/nagios/libexec/*.sh;
 else
   echo "dir '/usr/local/nagios/libexec/' does not exist";
 fi;
