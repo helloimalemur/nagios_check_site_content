d05647baab769b804702b52edebb4e6ad4b99c9f
James Koonts <jkoontsiii@gmail.com>
2023-01-23 20:16:22 -0500

    .

---
+++ src/examples/check_elastic.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./check_site "$CSUSER" "$CSCRED" https://localhost:9200 build_hash
--- src/examples/check_elasticsearch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./check_site_for_keyword "$CSUSER" "$CSCRED" https://localhost:9200 build_hash
